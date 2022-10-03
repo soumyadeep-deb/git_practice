8cb4008025076a464f7b788a8ac4010e87e1eea5
nt-soumyadeep <soumyadeep@neurapses.com>
2022-10-03 12:38:36 +0530

    Added btn javaScript to toggle heading

---
+++ index.js
@@ -0,0 +1,10 @@
+function btn1Click(){
+    let heading = document.querySelector('h1');
+
+    if (heading.style.display==='none'){
+        heading.style.display = 'block';
+    }
+    else{
+        heading.style.display = 'none';
+    }
+}
